b12bb7605558ef62698e30ed2c50b5e45a2014e4
Deej Potter <deejpotter@gmail.com>
2020-06-18 23:45:59 +1000

    Sorted out loading content in the routes render call

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { 
     aboutSection: {
-      bgColour: "primary",
-      bgImage: "images/laptop-60-opacity.png",
+      bgColour: "light",
       textColour: "dark",
-      sectionH2: "Deej Potter Designs",
-      sectionP: "Your own personal web designer. <br>Prices start at $100. <br><strong>Contact me now!</strong>",
+      bgImage: "",
+      sectionH2: "I’m here to help you",
+      sectionP: "I know there is a lot to learn when it comes to the internet so I’m here to guide you through the whole journey. You’ll have personal contact with me the whole time and I will be happy to answer any questions you have. I also have varying levels of service after your website is finished so you can be sure you will never be left not knowing what to do next.",
+      otherClasses: ""
+    },
+    secondSection: {
+      bgColour: "light",
+      textColour: "dark",
+      bgImage: "",
+      sectionH2: "I’m here to help you",
+      sectionP: "Every business needs a website. When people are looking for something these days the first place they look is online. So what do people see when they search for you? This is where I come in. I can build you a high quality, responsive website for as little as $100. You can then take your website and you’re free to do whatever you want with it but if you need help with that I offer a few levels of service from as little as $10 per month. You can read more about my prices on my pricing page.",
       otherClasses: ""
     }
   });
